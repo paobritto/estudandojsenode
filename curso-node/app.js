c5d2906f3e107a3df3de57937cc6e537a5536966
PAOLA  DE BRITTO <0221432611006@fatectq.local>
2026-08-06 09:02:18 -0300

    Aula inicial

---
+++ curso-node/app.js
@@ -0,0 +1,18 @@
+//console.log("Olá Mundo!");
+
+//let nome = "João";
+//console.log(nome);
+
+//console.log("Mensagem");
+
+//let nome = "Maria";
+//let idade = 25;
+//console.log(nome, idade);
+
+
+let a = 10;
+let b = 5;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
---
+++ curso-node/exercicios.js
@@ -0,0 +1,16 @@
+//exercício1
+//console.log("Meu primeiro proograma em Node.js");
+
+//exercício2
+//let nome = "Paola";
+//let idade = 18;
+//let cidade = "Taquaritinga";
+//console.log(nome, idade, cidade);
+
+//exercício3
+let G = 5;
+let P = 3;
+console.log(G + P);
+console.log(G - P);
+console.log(G * P);
+console.log(G / P);
