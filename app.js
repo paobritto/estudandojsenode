fceab3be5ee5b59e820f81e398d9db579294edd6
PAOLA  DE BRITTO <0221432611006@fatectq.local>
2026-08-11 11:26:16 -0300

    Atualizações

---
+++ app.js
@@ -0,0 +1,55 @@
+//const nome = "Paola";
+//const idade = 18;
+//const estudando = true; //Variável boolean (verdadeiro ou falso)
+
+//console.log(nome);   
+//console.log(idade);
+//console.log(estudando);
+//Nesse caso o node vai reconhecer o valor de cada variável, por ex, no caso da variável nome o valor é "paola" ent ele vai retornar "paola".
+
+//console.log(typeof nome);
+//console.log(typeof idade);
+//console.log(typeof estudando);
+//Nesse caso o node vai reconhecer o tipo da variável, então em vez dele retornar o valor armazenado nela, ele retorna o tipo, por ex, na variável nome, o node retornaria "string".
+
+//let a = 10;
+//let b = 3;
+
+//console.log(a%b); 
+//Essa operação matemática é o resto da divisão, (10/3=9, mas, sobra 1)
+
+//let numero =10;
+//console.log(numero%2);
+
+//const a = "10";
+//const b = "5";
+
+//console.log(a + b);
+//Nesse caso como os números estão com aspas o node entende que se trata de uma string ent ele não faria a adição e retornaria 15, e sim 105, ele vai literalmente ó juntar esses dois valores. MAS, ESSE DETALHE SÓ VALE PARA A ADIÇÃO, o restante das operações o javascript entende que c quer fazer uma operação matemática.
+
+//const idade = 18;
+//console.log(idade >= 18);
+//Aqui o resultado será : true
+
+//const idade = 15;
+//console.log(idade >= 18);
+//Aqui o resultado será false, estamos usando o tipo boolean.
+
+//const idade = 18;
+//if (idade >= 40) {
+  //  console.log("Pode entrar"); }
+
+//=== isso significa :exatamente igual
+
+const numero =10;
+ if(numero > 5){
+    console.log("O número é maior que 5")
+ }
+ else{
+    console.log("O número não é maior que 5")
+
+ }
+
+ //10 == "10" Nesse caso o js faz uma conversão e pergunta se eles podem ser usados na mesma maneira, e se converter o 10string para 10, ele fica igual ao outro.
+
+ 
